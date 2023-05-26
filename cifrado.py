7b05cdaab763f8ccace3c2dadaf2c2b0b1f4b60c
souljin34 <104099896+Ncaffry@users.noreply.github.com>
2023-05-26 22:19:47 +0200

    Update cifrado.py

--- cifrado.py
+++ cifrado.py
@@ -10,28 +10,27 @@ def cifrado():
 
  frase=str(frase)
 
- cifrar=list(frase)
-  
  fin_cifrado=[]
-
- for letra in cifrar:
-    try:
-     valor=Abecedario.index(letra)
-     clave=int(clave)
-     valor=int(valor)
-     valor+=clave
-     if valor>27:
+ for letra in frase:
+    if letra.isdigit():
+       fin_cifrado.append(letra)
+    elif letra.isspace():
+      fin_cifrado.append(" ")
+    else:
+      valor=Abecedario.index(letra)
+      clave=int(clave)
+      valor=int(valor)
+      valor+=clave
+      if valor>26:
        valor=int(valor)
        nvalor=valor-27
-       nvalor=str(nvalor)
+       nvalor=int(nvalor)
        total=Abecedario[nvalor]
        fin_cifrado.append(total)
-     else:
-      total=Abecedario[valor]
-      fin_cifrado.append(total)
-    except:
-       valor=0
- final_cifrado="".join(fin_cifrado)
+      else:
+       total=Abecedario[valor]
+       fin_cifrado.append(total)
+ final_cifrado = "".join(str(elemento)for elemento in fin_cifrado)
 
  print(final_cifrado)
  
@@ -41,37 +40,40 @@ def cifrado():
 
 def descifrar():
    
-   Abecedario=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',"ñ",'o', 'p', 'q', 'r',"s", 't', 'u', 'v', 'w', 'x', 'y', 'z']
+ Abecedario=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',"ñ",'o', 'p', 'q', 'r',"s", 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-   frase=input("Pon la frase que quieras descifrar, no añadas numeros: ")
+ frase=input("Pon la frase que quieras descifrar,no añadas numeros: ")
 
-   
-   clave=input("Qual clave? ")
-   
-   frase=str(frase)
+ clave=input("Qual clave? ")
 
-   cifrar=list(frase)
+ frase=str(frase)
 
-   fin_cifrado=[]
-
-   for letra in cifrar:
-    try:
-     valor=Abecedario.index(letra)
-     valor=int(valor)
-     clave=int(clave)
-     valor-=clave
-     if valor>27:
+ fin_cifrado=[]
+ for letra in frase:
+    if letra.isdigit():
+       fin_cifrado.append(letra)
+    elif letra.isspace():
+      fin_cifrado.append(" ")
+    else:
+      valor=Abecedario.index(letra)
+      clave=int(clave)
+      valor=int(valor)
+      valor-=clave
+      if valor>26:
        valor=int(valor)
        nvalor=valor-27
+       nvalor=int(nvalor)
        total=Abecedario[nvalor]
        fin_cifrado.append(total)
-     else:
-      total=Abecedario[valor]
-      fin_cifrado.append(total)
-    except:
-       valor=0
-   final_cifrado="".join(fin_cifrado)
-   print(final_cifrado)
+      else:
+       total=Abecedario[valor]
+       fin_cifrado.append(total)
+ final_cifrado = "".join(str(elemento)for elemento in fin_cifrado)
+
+ print(final_cifrado)
+ 
+    
+
 opcion=input("Que quieres cifrar o descifrar. ")
 if opcion=="cifrar":
   cifrado()
